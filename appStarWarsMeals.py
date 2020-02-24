b726c0ef4482731e87989f75ad15204080b204fc
RyanLegits <60904044+RyanLegits@users.noreply.github.com>
2020-02-23 16:41:28 -0800

    Add dishes for all characters
    In appStarWarsMeals.py:
    
    Parsing 'nationalDishes' data to find dish for each character.
    
    Removed 'newHomeworld' from 'Character' attributes which is not actually needed by the user, just by the program. Replaced it with 'dish' attribute, which the user will actually see.
    
    Removed "Section 1" comments, since program is basically complete in this version.
    
    Cleaned up unneeded code that was commented out, and cleaned up general formatting.

--- appStarWarsMeals.py
+++ appStarWarsMeals.py
@@ -3,105 +3,122 @@ import re
 import json
 import random
 
-#Star Wars Meals: Find what's for dinner -- IN SPACE!
+# Star Wars Meals: Find what's for dinner -- IN SPACE!
 
-#Retrieve all people from swapi
+# Retrieve all people from swapi
 people = swapi.get_all("people")
 
-#Total number of characters
-characterCount = people.count()
-
-#New class of characters
+# New class of characters
 class Character:
-	def __init__(self, name, homeworld, newHomeworld):
-		self.name = name
-		self.homeworld = homeworld
-		self.newHomeworld = newHomeworld
+    def __init__(self, name, homeworld, dish):
+        self.name = name
+        self.homeworld = homeworld
+        self.dish = dish
 
-###########################################1
-# Finding 'newHomeworld' and then creating
-# a list of Character objects
-###########################################1
 
-#List to index character objects; also used to populate "Character"s
+# List to index character objects; also used to populate "Character"s
 characterList = []
 
-#Function to amend keywords that don't match World Factbook API
+
+# Function to amend keywords that don't match World Factbook API
 def fixBrokenKeyword(brokenKeyword, list, newKeyword):
-	if brokenKeyword in list:
-		if newKeyword in brokenKeyword:
-			list.remove(brokenKeyword)
-			list.append(newKeyword)
-		else:
-			list.append(newKeyword)
+    if brokenKeyword in list:
+        if newKeyword in brokenKeyword:
+            list.remove(brokenKeyword)
+            list.append(newKeyword)
+        else:
+            list.append(newKeyword)
 
-#swapi function to iterate over all people
-for v in people.iter():
-	
-	#Setting up 'homeWorld' format for use in Character
 
-	n = v.homeworld
-	#Strip homeworld to integer since swapi returns a url
-	o = re.sub ('[^0-9]', '', n)
+#Importing data from json files
 
-	p = swapi.get_planet(o)
-	#Get actual planet name
-	q = p.name
+# World Factbook API
+with open('/Users/eflash/Documents/devProjects/appStarWarsMeals/worldFactbook.json', \
+          'r') as f:
+    countries_dict = json.load(f)
 
-	#Find new homeworld
+# National Dishes
+with open('/Users/eflash/Documents/devProjects/appStarWarsMeals/nationalDishes.json', \
+          'r') as f:
+    dictDishes = json.load(f)
 
-	#Contains all climate and terrain keywords combined for each planet
-	planetClimateList = []
-	#Loops to strip climates and terrains into single keywords
-	for i in p.climate.strip(",").split(" "):
-		planetClimateList.append(i)
+dictDishes = {k.lower(): v for k, v in dictDishes.items()}
 
-	for i in p.terrain.strip(",").split(" "):
-		planetClimateList.append(i)
+#Getting character data
 
-	fixBrokenKeyword('frozen', planetClimateList, 'freeze')
-	fixBrokenKeyword('windy', planetClimateList, 'wind')
-	fixBrokenKeyword('artificial temperate', planetClimateList, 'temperate')
-	fixBrokenKeyword('frigid', planetClimateList, 'bitter')
-	#Since 'superheated' called twice, torrid is used first in the function,
-	#because function won't then remove 'superheated' from planetClimateList,
-	#which allows 'heat' to also be added to list
-	fixBrokenKeyword('superheated', planetClimateList, 'torrid')
-	fixBrokenKeyword('superheated', planetClimateList, 'heat')
-	fixBrokenKeyword('subartic', planetClimateList, 'subarctic')
-	fixBrokenKeyword('artic', planetClimateList, 'arctic')
+# swapi function to iterate over all people
+for person in people.iter():
 
-	#World Factbook API
-	with open('/Users/eflash/Documents/devProjects/appStarWarsMeals/worldFactbook.json', \
-		'r') as f:
-		countries_dict = json.load(f)
-	
-	#Find candidateCountries by:
-	#Seeing how many keywords from planetClimateList match with each country in World
-	#Factbook API, then taking all countries with the max matched words, then choosing
-	#one of those countries at random to be the 'newHomeworld'
-	candidateCountries = {}
-	for countryMatch in countries_dict["countries"]:
-		matchCounter = 0
-		for climateKeyword in planetClimateList:
-			if climateKeyword in countries_dict.get("countries").get(countryMatch).get("data")\
-			.get("geography").get("climate", '').lower() or climateKeyword in countries_dict\
-			.get("countries").get(countryMatch).get("data")\
-			.get("geography").get("terrain", '').lower():
-				matchCounter += 1
+    # Setting up 'homeworld' format for use in Character
 
-		if matchCounter > 0:
-			candidateCountries[countryMatch] = matchCounter
-	#Exception for if no match between planet and countries
-	if not candidateCountries:
-		candidateCountries['default'] = '1'
-	
-	newHomeworld = random.choice([key for m in [max(candidateCountries.values())] for key,\
-		val in candidateCountries.items() if val == m])
+    rawHomeworld = person.homeworld
+    # Strip homeworld to integer since swapi returns a url
+    strippedHomeworld = re.sub('[^0-9]', '', rawHomeworld)
 
-	#Creating new characters and indexing them in list
-	characterList.append(Character(v.name, q, newHomeworld))
+    dataPlanet = swapi.get_planet(strippedHomeworld)
+    # Get actual planet name
+    characterHomeworld = dataPlanet.name
 
-###############1
-#End section 1
-###############1
+    # Find new homeworld
+
+    # Contains all climate and terrain keywords combined for each planet
+    planetClimateList = []
+    # Loops to strip climates and terrains into single keywords
+    for i in dataPlanet.climate.strip(",").split(" "):
+        planetClimateList.append(i)
+
+    for i in dataPlanet.terrain.strip(",").split(" "):
+        planetClimateList.append(i)
+
+    fixBrokenKeyword('frozen', planetClimateList, 'freeze')
+    fixBrokenKeyword('windy', planetClimateList, 'wind')
+    fixBrokenKeyword('artificial temperate', planetClimateList, 'temperate')
+    fixBrokenKeyword('frigid', planetClimateList, 'bitter')
+    # Since 'superheated' called twice, torrid is used first in the function,
+    # because function won't then remove 'superheated' from planetClimateList,
+    # which allows 'heat' to also be added to list
+    fixBrokenKeyword('superheated', planetClimateList, 'torrid')
+    fixBrokenKeyword('superheated', planetClimateList, 'heat')
+    fixBrokenKeyword('subartic', planetClimateList, 'subarctic')
+    fixBrokenKeyword('artic', planetClimateList, 'arctic')
+
+    # Find candidateCountries by:
+    # Seeing how many keywords from planetClimateList match with each country in World
+    # Factbook API, then taking all countries with the max matched words
+    candidateCountries = {}
+    for countryMatch in countries_dict["countries"]:
+        matchCounter = 0
+        for climateKeyword in planetClimateList:
+            if climateKeyword in countries_dict.get("countries").get(countryMatch).get("data") \
+                    .get("geography").get("climate", '').lower() or climateKeyword in countries_dict \
+                    .get("countries").get(countryMatch).get("data") \
+                    .get("geography").get("terrain", '').lower():
+                matchCounter += 1
+
+        if matchCounter > 0:
+            candidateCountries[countryMatch] = matchCounter
+    # Exception for if no match between planet and countries
+    if not candidateCountries:
+        candidateCountries['default'] = '1'
+
+    #'newHomeworld' generated randomly from candidateCountries
+    newHomeworld = random.choice([key for m in [max(candidateCountries.values())] for key, \
+                    val in candidateCountries.items()
+                    if val == m])
+
+    # Return national dish of newHomeworld
+    dishes = dictDishes.get(newHomeworld)
+    if dishes == None:
+        dishes = "Country not found!"
+
+    #Split dishes into multiple strings if more than one
+    dishesList = []
+    for i in dishes.split(", "):
+        dishesList.append(i)
+
+    #Choose random dish from country's dishes
+    dish = random.choice(dishesList)
+
+
+    # Creating new characters and indexing them in list
+    characterList.append(Character(person.name, characterHomeworld, dish))
